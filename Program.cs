e00481969776bdd66acdaba810ff97c13b8e278e
JJ Bussert <jj.bussert@gmail.com>
2023-03-25 22:07:08 -0500

    updated solution to .net 7

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
 ﻿using Statiq.App;
 using Statiq.Common;
 using Statiq.Web;
-using System;
-using System.Threading.Tasks;
 
-namespace Blog
-{
-    public class Program
-    {
-        public static async Task<int> Main(string[] args) =>
-          await Bootstrapper
-            .Factory
-            .CreateWeb(args)
-            .AddSetting(WebKeys.Xref, Config.FromDocument(doc => doc.Destination.FileNameWithoutExtension))
-            .RunAsync();
-    }
-}
+await Bootstrapper
+  .Factory
+  .CreateDefault(args)
+  .AddWeb()
+  // Add a setting that will ensure that markdown xref links resolve successfully
+  .AddSetting(WebKeys.Xref, Config.FromDocument(doc => NormalizedPath.ReplaceInvalidPathChars(((string)doc.Destination.FileNameWithoutExtension))))
+  .AddTabGroupShortCode()
+  .RunAsync();
