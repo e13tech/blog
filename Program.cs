3beb4a5a7bdbc30206df6a5fab8804f96668e8d6
JJ Bussert <jj.bussert@gmail.com>
2024-09-05 16:59:46 -0500

    convert to top level program.cs

--- Program.cs
+++ Program.cs
@@ -4,15 +4,8 @@ using Statiq.Web;
 using System;
 using System.Threading.Tasks;
 
-namespace Blog
-{
-    public class Program
-    {
-        public static async Task<int> Main(string[] args) =>
-          await Bootstrapper
-            .Factory
-            .CreateWeb(args)
-            .AddSetting(WebKeys.Xref, Config.FromDocument(doc => doc.Destination.FileNameWithoutExtension))
-            .RunAsync();
-    }
-}
+await Bootstrapper
+    .Factory
+    .CreateWeb(args)
+    .AddSetting(WebKeys.Xref, Config.FromDocument(doc => doc.Destination.FileNameWithoutExtension))
+    .RunAsync();
