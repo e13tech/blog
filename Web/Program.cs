6834fabb2bc2cac3590ab4e4eb8680286a7665b3
JJ Bussert <jj.bussert@gmail.com>
2024-10-10 08:26:58 -0500

    new instagram shortcode

--- Web/Program.cs
+++ Web/Program.cs
@@ -3,9 +3,11 @@ using Statiq.Common;
 using Statiq.Web;
 using System;
 using System.Threading.Tasks;
+using Web;
 
 await Bootstrapper
     .Factory
     .CreateWeb(args)
     .AddSetting(WebKeys.Xref, Config.FromDocument(doc => doc.Destination.FileNameWithoutExtension))
+    .AddShortcode<InstagramShortcode>("instagram")
     .RunAsync();
